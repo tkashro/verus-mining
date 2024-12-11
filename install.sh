ebe4b51b8da290006c100c48a142c663bd04e456
thomas <7k45hr0@gmail.com>
2024-12-12 08:26:31 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
-wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-mkdir ~/ccminer
+
+# Setup
+pkg update -y
+pkg upgrade -y
+pkg install libjansson wget nano git screen jq -y
+
+if [ ! -d ~/.ssh ]
+then
+  mkdir ~/.ssh
+  chmod 0700 ~/.ssh
+  cat << EOF > ~/.ssh/authorized_keys
+ssh-rsa KEY_HERE>
+EOF
+  chmod 0600 ~/.ssh/authorized_keys
+fi
+
+# Create ccminer directory
+if [ ! -d ~/ccminer ]
+then
+  mkdir ~/ccminer
+fi
 cd ~/ccminer
+
+
 GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/Oink70/Android-Mining/releases?per_page=1" | jq -c '[.[] | del (.body)]')
 GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .browser_download_url")
 GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .name")
