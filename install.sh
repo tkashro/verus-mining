25b1b9ff57ace4b9ccf4f9fddb35671e207243e6
thomas <7k45hr0@gmail.com>
2024-12-12 12:47:18 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,12 +31,11 @@ then
 fi
 cd ~/ccminer
 
-
-GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/Oink70/CCminer-ARM-optimized/releases?per_page=1" | jq -c '[.[] | del (.body)]')
+GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/tkashro/verus-mining/releases?per_page=1" | jq -c '[.[] | del (.body)]')
 GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets[0].browser_download_url")
 GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets[0].name")
 
-echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
+echo "Downloading latest pre-compiled version of CCminer: $GITHUB_DOWNLOAD_NAME"
 
 wget ${GITHUB_DOWNLOAD_URL} -P ~/ccminer
 
@@ -61,24 +60,26 @@ then
     fi
   done
 fi
-wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json -P ~/ccminer
+
+wget https://raw.githubusercontent.com/tkashro/verus-mining/refs/heads/master/config.json -P ~/ccminer
 
 if [ -f ~/ccminer/ccminer ]
 then
   mv ~/ccminer/ccminer ~/ccminer/ccminer_old
 fi
+
 mv ~/ccminer/${GITHUB_DOWNLOAD_NAME} ~/ccminer/ccminer
 chmod +x ~/ccminer/ccminer
 
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
-#exit existing screens with the name CCminer
+# exit existing screens with the name CCminer
 screen -S CCminer -X quit 1>/dev/null 2>&1
-#wipe any existing (dead) screens)
+# wipe any existing (dead) screens)
 screen -wipe 1>/dev/null 2>&1
-#create new disconnected session CCminer
+# create new disconnected session CCminer
 screen -dmS CCminer 1>/dev/null 2>&1
-#run the miner
+# run the miner
 screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n" 1>/dev/null 2>&1
 printf '\nMining started.\n'
 printf '===============\n'
@@ -88,14 +89,10 @@ printf 'stop: screen -X -S CCminer quit\n'
 printf '\nmonitor mining: screen -x CCminer\n'
 printf "exit monitor: 'CTRL-a' followed by 'd'\n\n"
 EOF
+
 chmod +x start.sh
 
-echo "setup nearly complete."
-echo "Edit the config with \"nano ~/ccminer/config.json\""
-
-echo "go to line 15 and change your worker name"
-echo "use \"<CTRL>-x\" to exit and respond with"
-echo "\"y\" on the question to save and \"enter\""
-echo "on the name"
-
-echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
+echo "===================================================="
+echo "NOTE: DO NOT FORGET TO EDIT THE CCMINER CONFIG"
+echo "Start the miner with \"cd ~/ccminer; ./start.sh\"."
+echo "===================================================="
